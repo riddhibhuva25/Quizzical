5dd8e0051b08e4f86d6d43eaf51a8eaaa008a619
Riddhi <bhuvariddhi25@gmail.com>
2024-12-10 19:02:30 +0530

    Riddhi

--- app/src/main/java/com/example/quizzical/MainActivity.kt
+++ app/src/main/java/com/example/quizzical/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.example.quizzical
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.example.quizzical.adapter.ViewPagerAdapter
+import com.example.quizzical.databinding.ActivityMainBinding
+import com.example.quizzical.fragments.HomeFragment
+import com.example.quizzical.fragments.ProfileFragment
+import com.example.quizzical.fragments.SettingFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        window.statusBarColor = this.resources.getColor(R.color.orange)
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+        val fragments = listOf(HomeFragment(), ProfileFragment(), SettingFragment()) // Replace with your fragment instances
+        val adapter = ViewPagerAdapter(fragments, this)
+        viewPager.adapter = adapter
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.profile -> {
+                    Toast.makeText(this,"profile",Toast.LENGTH_SHORT).show()
+                    viewPager.currentItem = 0
+
+
+                    true
+                }
+                R.id.home -> {
+                    Toast.makeText(this,"home",Toast.LENGTH_SHORT).show()
+                    viewPager.currentItem = 1
+                    true
+                }
+
+                R.id.setting-> {
+                    Toast.makeText(this,"setting",Toast.LENGTH_SHORT).show()
+                    viewPager.currentItem = 2
+                    true
+                }
+                else -> false
+            }
+        }
     }
-}
+
+    }
--- app/src/main/java/com/example/quizzical/registration/SignUpActivity.kt
+++ app/src/main/java/com/example/quizzical/registration/SignUpActivity.kt
@@ -1,15 +1,20 @@
 package com.example.quizzical.registration
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quizzical.MainActivity
 import com.example.quizzical.R
 import com.example.quizzical.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    var isAllFieldsChecked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,5 +26,61 @@ class SignUpActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initialization()
+    }
+
+    private fun initialization() {
+
+
+        binding.btnLogin.setOnClickListener {
+            isAllFieldsChecked = CheckAllFields()
+
+            if (isAllFieldsChecked) {
+
+                startActivity(Intent(this, MainActivity::class.java))
+
+            }
+        }
+
+
+        binding.txtLogInSmall.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+
+        }
+
+    }
+
+
+    private fun CheckAllFields(): Boolean {
+        if (binding.edtFirstName.length() == 0){
+            binding.edtFirstName.error = "Enter your name"
+            Toast.makeText(this,"Please enter your first name", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (binding.edtLastName.length() == 0){
+            binding.edtLastName.error = "Enter your name"
+            Toast.makeText(this,"Please enter your last name", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (binding.edtEmailSignUp.length() == 0) {
+            binding.edtEmailSignUp.error = "Email is required"
+            Toast.makeText(this,"Please enter your email id", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        if (binding.edtPassSignUp.length() == 0) {
+            binding.edtPassSignUp.error = "Password is required"
+            Toast.makeText(this,"Please enter your password", Toast.LENGTH_SHORT).show()
+            return false
+        } else if (binding.edtPassSignUp.length() < 8) {
+            binding.edtPassSignUp.error = "Password must be minimum 8 characters"
+            Toast.makeText(this,"Please enter your 8 digit password", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        // after all validation return true.
+        return true
     }
 }
